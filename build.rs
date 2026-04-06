a52346ac204a531a0d8fd1c0587ed853a82213d9
moumoudark190-cloud <moumoudark190@gmail.com>
2026-04-06 16:29:31 +0100

    Update build.rs

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@
 fn main() {
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
-        res.set_icon("assets/logo.ico");   // path to your .ico file
+        res.set_icon("assets/logo.ico");
         res.compile().unwrap();
     }
 }
